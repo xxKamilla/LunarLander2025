520741f35762147dd115adab54782bd55f8b01d1
Halvorhaand <halhaa00921@stud.noroff.no>
2025-01-17 15:13:46 +0100

    LanderController testing

--- Assets/Scripts/SpaceShip/LanderController.cs
+++ Assets/Scripts/SpaceShip/LanderController.cs
@@ -6,5 +6,42 @@ using UnityEngine.InputSystem;
 /// </summary>
 public class LanderController : MonoBehaviour
 {
+   
+    public SpaceShipInput input;
+    public float yaw;
+    public float pitch;
+    public float roll;
 
+
+    public void Awake()
+    {
+        input = new SpaceShipInput();
+    }
+
+    public void Update()
+    {
+        transform.Rotate(pitch, roll, yaw, Space.Self);
+    }
+    private void OnEnable()
+    {
+        input.Enable();
+        input.Gameplay.Enable();
+        input.Gameplay.ShipRotation.performed += ShipRotationStarted;
+
+    }
+    private void OnDisable()
+    {
+        input.Gameplay.Disable();
+    }
+
+    private void ShipRotationStarted(InputAction.CallbackContext obj)
+    {
+        yaw = obj.ReadValue<Vector2>().y;
+        pitch = obj.ReadValue<Vector2>().x;
+    }
+
+    private void ShipRotationCanceled(InputAction.CallbackContext obj)
+    {
+
+    }
 }
