c43552089b45a78a90019f1fcc5ce97c56cf01da
paulborg <apurplegoat.temp@gmail.com>
2025-01-17 15:37:01 +0100

    Simple thruster

--- Assets/Scripts/SpaceShip/Thrusters.cs
+++ Assets/Scripts/SpaceShip/Thrusters.cs
@@ -3,7 +3,44 @@ using UnityEngine.InputSystem;
 /// <summary>
 /// Ship thrusters and input.
 /// </summary>
+
 public class Thrusters : MonoBehaviour
 {
+    public BallGravity ballGravity;
+    public FuelTank fuelTank;
+    public Vector3 upDirection;
+
+    private void Awake()
+    {
+        ballGravity = GetComponent<BallGravity>();
+        fuelTank = GetComponent<FuelTank>();
+    }
+
+    private void Update()
+    {
+        ballGravity.thrust = transform.up;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ThrusterOn();
+        }
+
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            ThrusterOff();
+        }
+    }
+
+    private void ThrusterOn()
+    {
+        Debug.Log("ThrusterOn");
+        ballGravity.thrust = transform.up * 20;
+    }
+
+    private void ThrusterOff()
+    {
+        Debug.Log("ThrusterOff");
+        ballGravity.thrust = transform.up;
+    }
 
 }
