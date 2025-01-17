2bc11cfbc9c79d3f272b2f0791066807a9d469c7
OdinIMG2 <Odinhegdahl@hotmail.com>
2025-01-17 15:51:00 +0100

    Fuel tank Update

--- Assets/Scripts/SpaceShip/FuelTank.cs
+++ Assets/Scripts/SpaceShip/FuelTank.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 /// </summary>
 public class FuelTank : MonoBehaviour
 {
+
+    
     public float maxFuel;
     public float currentFuel;
-    
+   
     
     
     // Start is called before the first frame update
     void Start()
     {
+        maxFuel = 100;
+        currentFuel = maxFuel;
         
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+   
+    bool burnFuel(float burnRate)
+    { 
+    
+        if (currentFuel > 0)
+        {
+            currentFuel -= burnRate;
+            return true;   
+        }
+        else
+        {
+            return false;
+        }
+
+
     }
+
+
+
 }
--- Assets/Scripts/SpaceShip/Thrusters.cs
+++ Assets/Scripts/SpaceShip/Thrusters.cs
@@ -43,4 +43,7 @@ public class Thrusters : MonoBehaviour
         ballGravity.thrust = transform.up;
     }
 
+
+
+
 }
