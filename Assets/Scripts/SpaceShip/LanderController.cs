4e5f88afba9bfa0b854cb308d2a13208b4d71298
Halvorhaand <halhaa00921@stud.noroff.no>
2025-01-17 16:15:13 +0100

    Rotation

--- Assets/Scripts/SpaceShip/LanderController.cs
+++ Assets/Scripts/SpaceShip/LanderController.cs
@@ -11,6 +11,10 @@ public class LanderController : MonoBehaviour
     public float yaw;
     public float pitch;
     public float roll;
+    public float rotationTorque = 10;
+    public Vector3 curretRot;
+    public bool isRotation;
+    
 
 
     public void Awake()
@@ -20,7 +24,18 @@ public class LanderController : MonoBehaviour
 
     public void Update()
     {
-        transform.Rotate(pitch, roll, yaw, Space.Self);
+        // adding to the rotation each frame
+        curretRot.x += pitch * Time.deltaTime;
+        curretRot.y += roll * Time.deltaTime;
+        curretRot.z += yaw * Time.deltaTime;
+
+        // Rotates the transform 
+        transform.Rotate(curretRot.x, curretRot.y, curretRot.z, Space.Self);
+        //curretRot = Vector3.Lerp(curretRot, Vector3.zero, 0.01f);
+    }
+    public void LateUpdate()
+    {
+        curretRot = Vector3.Lerp(curretRot, Vector3.zero, 0.003f);
     }
     private void OnEnable()
     {
@@ -36,8 +51,9 @@ public class LanderController : MonoBehaviour
 
     private void ShipRotationStarted(InputAction.CallbackContext obj)
     {
-        yaw = obj.ReadValue<Vector2>().y;
-        pitch = obj.ReadValue<Vector2>().x;
+        // changes the rotation based on the input vector2 value. 
+        yaw = obj.ReadValue<Vector2>().x;
+        pitch = obj.ReadValue<Vector2>().y;
     }
 
     private void ShipRotationCanceled(InputAction.CallbackContext obj)
