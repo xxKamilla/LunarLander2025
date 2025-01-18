3e5dd07d33535b5e1d9eb0c2bf983cd759e3b8b2
Robgunwer <robgunwer@gmail.com>
2025-01-18 22:36:57 +0100

    testing fall damage

--- Assets/Scripts/SpaceShip/Hull.cs
+++ Assets/Scripts/SpaceShip/Hull.cs
@@ -1,4 +1,6 @@
+using Unity.VisualScripting;
 using UnityEngine;
+
 
 
 public class Hull : MonoBehaviour
@@ -8,22 +10,34 @@ public class Hull : MonoBehaviour
     /// Script handeling health and damage taken
     /// 
     /// Update 17.01.2025 - Robin
-    /// 
+    /// main outline
+    /// Update 18.01.2025 - Robin
+    /// changing some of the methods
     /// </summary>
 
 
+    
 
     public int maxHealth;
     public int remainingHealth;
     public int lives;
-    
+
+    public BallGravity gravity;
+
+
+    private void Awake()
+    {
+        gravity = GetComponent<BallGravity>();
+    }
+
 
 
     private void Start()
     {
         //Get the current stats fo the spaceship
         // TODO : assign different values to different spaceships
-
+        // upgrades 1-5 ? and increase by 20 or 50 per upgrade
+        
         maxHealth = 100; 
         remainingHealth = maxHealth;
         lives = 5; // should we add lives ?
@@ -32,11 +46,11 @@ public class Hull : MonoBehaviour
 
   
 
-    void HealthLost(int damageTaken)
+    void HealthLost(int damageTaken)     
+    {
         // handeling damage taken by checking if the player has more heatlth left
         // requires int input for the damage taken
-    {
-       
+
         if (remainingHealth <= 0)
         { 
             Death();
@@ -47,11 +61,11 @@ public class Hull : MonoBehaviour
     }
 
     void HealthGained(int healthGained)
-    // handeling damage taken by checking if the player has more heatlth left
-    // requires int input for the damage taken
     {
+        // handeling damage taken by checking if the player has more heatlth left
+        // requires int input for the damage taken
 
-      if (remainingHealth >= maxHealth)
+        if (remainingHealth >= maxHealth)
         {
             remainingHealth = maxHealth;
         }
@@ -60,16 +74,47 @@ public class Hull : MonoBehaviour
 
     }
 
-    void DamageTaken(float dmg)
+    public void FallDamage()
     {
-        //TODO: calculate the damage taken based on velocity or asteroide inpact
+        //TODO: calculate the damage taken based on velocity or impact
         //then send the damage to the *HealthLost* method 
-        //placeholder
-        HealthLost(6);
+        
+
+        // update 18.01.2025
+
+        Vector3 speed = gravity.velocity;
+
+        float dmg = speed.x + speed.z + speed.y;
+        int damageScale;
+        //tested fall damage, seems to work but you need to invoke the function through somewhere else, like the gravity script
+        //placeholder which works
+        if (dmg <= -20f)
+        {
+            damageScale = 50;
+        }
+        else if (dmg <= -10f)
+        {
+            damageScale = 20;
+        }
+        else if (dmg >= 20f)
+        {
+            damageScale = 50;
+        }
+        else if (dmg >= 10f)
+        {
+            damageScale = 20;
+        }
+        else
+        {
+            damageScale = 0;
+        }
+
+
+        HealthLost(damageScale);
 
     }
 
-    void shipRepair(int repair)
+    void ShipRepair(int repair)
     {
         //TODO:  repair the ship 
         //landing pad ? or powerup in the air ?
@@ -85,5 +130,6 @@ public class Hull : MonoBehaviour
 
 
     }
+    
 
 }
