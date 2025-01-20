8a664c59526d388f69ae777df4f207bf9db04564
paulborg <apurplegoat.temp@gmail.com>
2025-01-20 14:47:51 +0100

    Improved Thruster + Implemented Thruster Particle FX
    Thruster now was an adjustable intensity "build-up" and "decay" instead of jumping from 0-100% instantly.
    
    Particle VFX have been added to the lander and their emission rate is controlled by the adjustable thruster intensity, so they also gradually transition from off to max power, and then decay to zero.
    
    More tweaks needed, both for gameplay and visual aspects, but functionality is there and particle parameters are referenced & accessible, so these can be further adjusted as we continue working

--- Assets/Scripts/SpaceShip/Thrusters.cs
+++ Assets/Scripts/SpaceShip/Thrusters.cs
@@ -9,6 +9,15 @@ public class Thrusters : MonoBehaviour
     public BallGravity ballGravity;
     public FuelTank fuelTank;
     public Vector3 upDirection;
+    
+    public float currentThrust;
+    public float maxThrust;
+    public float thrustBuildRate;
+    public float thrustDecayRate;
+
+    public ParticleSystem firePS;
+    public ParticleSystem smokePS;
+    public ParticleSystem bSmokePS;
 
     private void Awake()
     {
@@ -18,28 +27,89 @@ public class Thrusters : MonoBehaviour
 
     private void Update()
     {
+
+        ParticleSystem.MainModule psFireMain = firePS.main;
+        ParticleSystem.EmissionModule psFireEmission = firePS.emission;
+
+        ParticleSystem.MainModule psSmokeMain = smokePS.main;
+        ParticleSystem.EmissionModule psSmokeEmission = smokePS.emission;
+
+        ParticleSystem.MainModule psBSmokeMain = bSmokePS.main;
+        ParticleSystem.EmissionModule psBSmokeEmission = bSmokePS.emission;
+
+
         ballGravity.thrust = transform.up;
+        float targetThrust = 0f;
 
         if (Input.GetKey(KeyCode.Space) && fuelTank.BurnFuel(0.1f * Time.deltaTime))
         {
-            ThrusterOn();
+            targetThrust = maxThrust;
+ 
         }
 
-        else if (Input.GetKeyUp(KeyCode.Space))
+        if (currentThrust < targetThrust)
         {
-            ThrusterOff();
+            currentThrust = Mathf.MoveTowards(currentThrust, targetThrust, thrustBuildRate * Time.deltaTime);
+            firePS.Play();
+            smokePS.Play();
+            bSmokePS.Play();
+            psFireEmission.rateOverTime = currentThrust / maxThrust * 1000;
+            psSmokeEmission.rateOverTime = currentThrust / maxThrust * 100;
+            psBSmokeEmission.rateOverTime = currentThrust / maxThrust * 100;
         }
+
+        else if (!Input.GetKey(KeyCode.Space))
+        {
+            currentThrust = Mathf.MoveTowards(currentThrust, targetThrust, thrustDecayRate * Time.deltaTime);
+            //firePS.Stop();
+            //smokePS.Stop();
+            //bSmokePS.Stop();
+            psFireEmission.rateOverTime = currentThrust / maxThrust * 500;
+            psSmokeEmission.rateOverTime = currentThrust / maxThrust * 50;
+            psBSmokeEmission.rateOverTime = currentThrust / maxThrust * 50;
+        }
+
+        ballGravity.thrust = transform.up * currentThrust;
+
     }
 
-    private void ThrusterOn()
-    {
-        Debug.Log("ThrusterOn");
-        ballGravity.thrust = transform.up * 20;
-    }
 
-    private void ThrusterOff()
-    {
-        Debug.Log("ThrusterOff");
-        ballGravity.thrust = transform.up;
-    }
+
+
+
+    #region Original Thruster
+    //private void Update()
+    //{
+    //    ballGravity.thrust = transform.up;
+
+    //    if (Input.GetKey(KeyCode.Space) && fuelTank.BurnFuel(0.1f * Time.deltaTime))
+    //    {
+    //        ThrusterOn();
+    //    }
+
+    //    else if (Input.GetKeyUp(KeyCode.Space))
+    //    {
+    //        ThrusterOff();
+    //    }
+    //}
+
+    //private void ThrusterOn()
+    //{
+    //    Debug.Log("ThrusterOn");
+    //    ballGravity.thrust = transform.up * 20;
+    //    thrusterVFX.SetActive(true);
+    //}
+
+    //private void ThrusterOff()
+    //{
+    //    Debug.Log("ThrusterOff");
+    //    ballGravity.thrust = transform.up;
+    //    thrusterVFX.SetActive(false);
+    //}
+    #endregion
+
+
+
+
+
 }
