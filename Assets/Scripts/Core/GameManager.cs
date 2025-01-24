5a5d954bb7d6a1f935d1b55034783d56e95f4fee
CgViking <bjorn.andre.cock@gmail.com>
2025-01-24 15:27:14 +0100

    Made a GameManager script
    Nothing in there currently, just the placeholder

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Core
+{
+    /// <summary>
+    /// Ass-director: This script will be in charge of keeping the game state, maybe even the score and other important stuff.
+    /// Maybe some functions on game end, game loop related.
+    /// </summary>
+    public class GameManager : MonoBehaviour
+    {
+        
+    }
+}
