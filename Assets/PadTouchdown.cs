20c0acc2eed328805f3dd86d630e4d05d051e062
CgViking <bjorn.andre.cock@gmail.com>
2025-01-30 16:52:51 +0100

    Update PadTouchdown.cs
    Removed some Using's that stopped me from building

--- Assets/PadTouchdown.cs
+++ Assets/PadTouchdown.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
-using UnityEditor.TerrainTools;
 using UnityEngine;
 
 public class PadTouchdown : MonoBehaviour
