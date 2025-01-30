a847b6c69c88b4ab82a2b776daee94b47df59180
OdinIMG2 <Odinhegdahl@hotmail.com>
2025-01-30 13:55:44 +0100

    Update Dmg

--- Assets/Scripts/SpaceShip/Dmg.cs
+++ Assets/Scripts/SpaceShip/Dmg.cs
@@ -1,9 +1,16 @@
+using Interfaces;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Dmg : MonoBehaviour
 {
+    private Hull hull;
+    public int damage = -1;
+    public int healing = 1;
+    private float drag;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +22,35 @@ public class Dmg : MonoBehaviour
     {
         
     }
+    private void OnTriggerEnter(Collider other)
+    {
+      if (other.TryGetComponent(out IPhysicsObject physicsObject))
+      {
+            physicsObject.Drag = drag;
+
+            if (damage < 0)
+            {
+                hull.HealthHandler(damage);
+                
+            }
+            else if (healing < 0)
+            {
+                hull.HealthHandler(healing);
+            }
+            
+      }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent(out IPhysicsObject physicsObject))
+        {
+            physicsObject.Drag = 0f;
+        }    
+    }
+        
+    
+
+
+
+
 }
